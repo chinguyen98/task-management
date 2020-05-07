bf43f197ff89d25f970884450349ff0b673b75bd
chinguyen98 <dacchi6761@gmail.com>
2020-05-07 16:29:05 +0700

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -11,5 +11,12 @@ export class AuthService {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto) {
+        const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+        if (!username) {
+            throw new UnauthorizedException('Username or password invalid!');
+        }
     }
 }
