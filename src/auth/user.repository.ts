bbcb8aac99290e447dbf35d2a9c1571397194e16
chinguyen98 <dacchi6761@gmail.com>
2020-05-07 09:53:32 +0700

    update

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,6 +12,14 @@ export class UserRepository extends Repository<User> {
         user.username = username;
         user.password = password;
 
-        await user.save();
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === 'ER_DUP_ENTRY') {
+                throw new ConflictException('Username already exists!');
+            }else{
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
