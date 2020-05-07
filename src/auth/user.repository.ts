7b3e9e443c98124c321ea3f54d08ffc7dac2b6fe
chinguyen98 <dacchi6761@gmail.com>
2020-05-07 10:35:31 +0700

    update

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,25 +1,33 @@
 import { Repository, EntityRepository } from "typeorm";
-import { User } from "./user.entity";
-import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
+        const salt = await bcrypt.genSalt();
+
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = salt;
+        user.password = await this.hashPassword(password, salt);
 
         try {
             await user.save();
         } catch (error) {
             if (error.code === 'ER_DUP_ENTRY') {
                 throw new ConflictException('Username already exists!');
-            }else{
+            } else {
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 }
