c76df40f2ed99e3d0c0be114612e66959980a0cf
chinguyen98 <dacchi6761@gmail.com>
2020-05-24 21:50:29 +0700

    update

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -3,7 +3,7 @@ import { IsOptional, IsIn, IsNotEmpty } from "class-validator";
 
 export class GetTaskFilterDto {
     @IsOptional()
-    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
+    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN], { message: 'Task status must be in: DONE, IN_PROGRESS, OPEN!' })
     status: TaskStatus;
 
     @IsOptional()
