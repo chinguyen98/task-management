d27cf1b2d7d6a2d056322360f2ab451fdd67bbbf
chinguyen98 <dacchi6761@gmail.com>
2020-05-28 22:38:48 +0700

    Add cors config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ dotenv.config('../.env');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
